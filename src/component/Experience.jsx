318721e50fdbbd797fe39a90ef2565ef76477290
Adil Shaikh <adilsk0219@gmail.com>
2022-12-29 16:36:37 +0530

    'change'

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -70,7 +70,7 @@ const Experience = () => {
 
 
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-10'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500 py-2'>Experience</p>
